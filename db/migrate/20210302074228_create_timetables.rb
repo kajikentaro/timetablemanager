14cc6e5ab0d31b55f5803b265ae409d0b333bad0
kajikentaro <k.kentarou0412@gmail.com>
2021-03-03 12:54:37 +0900

    fix migration file

--- db/migrate/20210302074228_create_timetables.rb
+++ db/migrate/20210302074228_create_timetables.rb
@@ -1,7 +1,9 @@
 class CreateTimetables < ActiveRecord::Migration[6.1]
   def change
     create_table :timetables do |t|
-
+      t.column :id, 'int primary key'
+      t.string :name
+      t.string :data
       t.timestamps
     end
   end
