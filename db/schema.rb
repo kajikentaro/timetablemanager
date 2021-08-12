ac00fa94402f3f6d69d9fe1a0d7c8af36e35cf7f
kajikentaro <k.kentarou0412@gmail.com>
2021-08-12 06:46:22 +0000

    use unicorn env

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_155412) do
+ActiveRecord::Schema.define(version: 2021_03_05_125111) do
 
   create_table "parties", force: :cascade do |t|
     t.string "name"
@@ -27,11 +27,6 @@ ActiveRecord::Schema.define(version: 2021_03_07_155412) do
     t.string "party"
     t.string "timetable"
     t.string "group"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "tmps", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
