a54181ad4efcc9d4cc5f59a6400e7adfd6f7c65d
kajikentaro <k.kentarou0412@gmail.com>
2021-08-26 05:31:44 +0000

    fix design layout

--- app/helpers/timetables_helper.rb
+++ app/helpers/timetables_helper.rb
@@ -4,31 +4,11 @@ module TimetablesHelper
     end
 
     def go_home_button
-        return button_to "最初に戻る", {controller: 'timetables', action: 'index'}, {method: :get, params:{num1:'fromTM'},class: 'button full'}
-    end
-
-
-    def view_result
-        return button_to "候補を見る", {controller: 'timetables', action: 'result'}, {method: :get, params:{num1:'fromTM'},class: 'button full'}
+        return button_to "最初に戻る", {controller: 'timetables', action: 'index'}, {method: :get, params:{num1:'fromTM'},class: 'button third'}
     end
 
     def view_history_back
-        return button_to "前の画面へ戻る", {controller: 'timetables', action: 'history'}, {method: :get, params:{num1:'fromTM'},class: 'button full'}
+        return button_to "前の画面へ戻る", {controller: 'timetables', action: 'history'}, {method: :get, params:{num1:'fromTM'},class: 'button third'}
     end
 
-    def view_distribution
-        return button_to "分布を見る", {controller: 'timetables', action: 'distribution'}, {method: :get, params:{num1:'fromTM'},class: 'button full'}
-    end
-
-    def go_home
-        return button_to "ホームへ", {controller: 'application', action: 'index'}, {method: :get, params:{num1:'fromTM'},class: 'button full'}
-    end
-
-    def go_setting
-        return button_to "グループを編集", {controller: 'timetables', action: 'setting'}, {method: :get, params:{num1:'fromTM'},class: 'button full'}
-    end
-
-    def view_history
-        return button_to "提出一覧を見る", {controller: 'timetables', action: 'history'}, {method: :get, params:{num1:'fromTM'},class: 'button full'}
-    end
 end
