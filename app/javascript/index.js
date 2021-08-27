9c05b2e03ab5bd2aceed2c4d33ba63fbd8791500
kajikentaro <k.kentarou0412@gmail.com>
2021-08-27 14:34:04 +0000

    fix a bug: can't click index button in smartphone layout

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,5 +1,9 @@
 console.log('hello');
 window.onload = ()=>{
+    document.getElementById('new-TT-sp').onclick = ()=>{
+        var url = document.getElementById('new-url').dataset.json;
+        location.href = url;
+    }
     document.getElementById('new-TT').onclick = ()=>{
         var url = document.getElementById('new-url').dataset.json;
         location.href = url;
