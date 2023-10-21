d5a461819528e2e6c651482d95c910fd6da39728
kajikentaro <kajindowsxp@gmail.com>
2023-10-21 22:13:14 +0900

    remove config.hosts on production.rb
    fix 403 issue caused by dns rebinding attack prevention

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts << "ttmanager.kajindowsxp.com"
-  config.hosts << "localhost"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
